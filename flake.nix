8c11f09c301fa1978342b53635c281056677c102
Sewer56 <sewer56lol@googlemail.com>
2025-10-10 22:40:49 +0100

    Added: NixOS flake for uasm

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "LZMA SDK sys bindings for Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          uasm
+        ];
+
+        shellHook = ''
+          export NIXPKGS_ALLOW_UNFREE=1
+        '';
+      };
+    };
+}
